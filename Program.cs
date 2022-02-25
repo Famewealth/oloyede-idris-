57a25f1699e92e3d9dcd88b7a7bee4937b906eb7
Famewealth <100274204+Famewealth@users.noreply.github.com>
2022-02-24 23:31:16 -0800

    C# assignment
    C# assignment

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = " Polyilaro";
+            // create variable name that accept integer,double and string input from the keyboard
+            Console.WriteLine(" Enter The New Integer Value");
+            int myint = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter The New Double Value");
+            double mydouble = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" Enter The New String Value");
+           string mystring = Console.ReadLine();
+            //concatinating and printing user input and constant data 
+            Console.WriteLine( i + myint);
+            Console.WriteLine( i +  mydouble);
+            Console.WriteLine( s + " " + mystring );
+            Console.ReadLine();
+        }
+    }
+}
